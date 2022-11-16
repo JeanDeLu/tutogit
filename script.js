27ba6f3e15b7117f05990ad7715d4e9302234f08
smjr <rabialahyandry123@gmail.com>
2022-11-16 09:22:45 +0200

    changement de code

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
-var sp,btn_start,btn_stop, t, ms, s, mn, h;
+var h=0;
+var min=0;
+var s=0;
+var ms=0;
 
-window.onload = function (){
-    sp= document.getElementsByTagName('span');
-    btn_start= document.getElementById('start');
-    btn_stop= document.getElementById('stop');
-    t;
-    ms =0, s=0,mn=0,h=0;
-}  
-function update_chono() {
+var intervalMiliseconde;
+var intervalSeconde;
+var intervalMinute;
+var intervalHeure;
+
+function setMilisegonde(){
+    if(ms==999)
+    {
+        ms=0;
+    }
     ms+=1;
-    if(ms == 10){
-        ms=1;
-        s+=1
-    }
-    if(s==60){
+  document.getElementById('ms').innerHTML= ms + ' ms';
+}
+function setSeconde(){
+    s+=1;
+    if(s==59)
+    {
         s=0;
-        mn+=1
     }
-    if(mn==60){
-        mn=0;
-        h+=1;
+  document.getElementById('s').innerHTML= s + ' s';
+}
+function setMinute(){
+    min+=1;
+    if(min==59)
+    {
+        min=0;
     }
-    sp[0].innerHTML = h + "h" ;
-    sp[1].innerHTML = min + "min" ;
-    sp[2].innerHTML = s + "s" ;
-    sp[3].innerHTML = ms + "ms" ;
+  document.getElementById('min').innerHTML= min+ ' min';
 }
-function start() {
-    t =setInterval( update_chono,100);
-    btn_start.disabled = true;
+function setHeure(){
+    
+  h+=1;
+    if(h==23)
+    {
+        h=0;
+    }
+  document.getElementById('h').innerHTML= h + ' h';
 }
-function stop() {
-    clearInterval(t);
-    btn_start.disabled=false;
+
+function start(){
+   intervalHeure = setInterval(setHeure,3600000);
+   intervalMinute = setInterval(setMinute,60000);
+   intervalSeconde = setInterval(setSeconde,1000);
+   intervalMiliseconde = setInterval(setMilisegonde,1);
 }
-function reset() {
-    clearInterval(t);
-    btn_start.disabled=false;
-    ms=0,s=0,mn=0,h=0;
-    sp[0].innerHTML = h + "h" ;
-    sp[1].innerHTML = min + "min" ;
-    sp[2].innerHTML = s + "s" ;
-    sp[3].innerHTML = ms + "ms" ;
+
+function stop(){
+    clearInterval(intervalHeure);;
+    clearInterval(intervalMinute);
+    clearInterval(intervalSeconde);
+    clearInterval(intervalMiliseconde);
 }
+
+function reset(){
+    stop();
+      h=0;
+      min=0;
+      s=0;
+      ms=0;
+
+      document.getElementById('h').innerHTML='0 h';
+      document.getElementById('min').innerHTML='0 min';
+      document.getElementById('s').innerHTML='0 s';
+      document.getElementById('ms').innerHTML='0 ms';
+
+}
+
